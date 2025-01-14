7a39509429cc02e781f51dcf691aa1e103338a45
foisal404 <foisalahmmed2@gmail.com>
2025-01-15 01:54:23 +0600

    json versel

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const port = process.env.PORT || 5000;
 
 
 //middleware
-// app.use(cors())
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
+app.use(cors())
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true,
+//     optionSuccessStatus: 200,
+// }
+// app.use(cors(corsOptions))
 
-// app.use(express.json())
+app.use(express.json())
 
 
 // dB Connection
